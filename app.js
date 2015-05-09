01c7fb096c5557e99e72959d8f563b3458f6bab2
nakulm95 <nakulm95@uw.edu>
2015-05-09 02:45:56 -0700

    setting up node

--- app.js
+++ app.js
@@ -9,3 +9,15 @@ var FB = new facebook.Facebook({
   version   :   'v2.3',
   secret    :   'f765517b760a787f24d9d2c569ff020a'
 });
+
+var serve = serveStatic('.', {});
+var port = process.env.PORT || 8081;
+
+var server = http.createServer(function(req, res) {
+  var _get = queryString.parse(url.parse(req.url).query);
+  var done = finalhandler(req, res);
+  serve(req, res, done);
+});
+
+server.listen(port);
+console.log("Server running at http://localhost:" + port);
