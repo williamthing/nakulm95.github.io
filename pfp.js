a19659fde91ca70b0569f00c55e0b8dc2d1ecdbf
nakulm95 <nakulm95@uw.edu>
2015-05-09 02:48:41 -0700

    more crap

---
+++ pfp.js
@@ -0,0 +1,17 @@
+(function() {
+    $(document).ready(function() {
+        contentLoad();
+    });
+
+    function contentLoad() {
+      /*
+        loadContent(function(pages) {
+            console.log(pages);
+        });*/
+      console.log("gg");
+    }
+
+    function loadContent(callback) {
+      
+    }
+})();
